e333d84d077224bef56bc1211367c782b35355f4
YulianiMurti <32427693+YulianiMurti@users.noreply.github.com>
2018-03-27 15:43:00 +0800

    Membuat Atribut dan Behavior

--- src/praktek01/Bunga.java
+++ src/praktek01/Bunga.java
@@ -10,5 +10,20 @@ package praktek01;
  * @author ASUS
  */
 public class Bunga {
+    String nama;
+    String iklim;
+    String ukuran;
+    String Warna_Kelopak;
+    double Jumlah_Kelopak;
+    String Jenis_Kelamin;
     
+    void CaraKawin(){
+        System.out.println("Nama Bunga     : "+nama);
+        System.out.println("Iklim          : "+iklim);
+        System.out.println("Ukuran         : "+ukuran);
+        System.out.println("Warna Kelopak  : "+Warna_Kelopak);
+        System.out.println("Jumlah Kelopak : "+Jumlah_Kelopak);
+        System.out.println("Jenis Kelamin  : "+Jenis_Kelamin);  
+        
+    }
 }
