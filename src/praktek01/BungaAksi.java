8ddb8a6da53b0e0b486045a80fa0570fb04fc704
YulianiMurti <32427693+YulianiMurti@users.noreply.github.com>
2018-03-27 16:25:14 +0800

    Perbaikan Mengisi nilai Atribut

--- src/praktek01/BungaAksi.java
+++ src/praktek01/BungaAksi.java
@@ -11,33 +11,16 @@ package praktek01;
  */
 public class BungaAksi {
     public static void main(String[] args) {
-        Bunga Sepatu = new Bunga();
+        
         Bunga Kertas = new Bunga();
-        Bunga Melati = new Bunga();
         
-        Sepatu.nama = "Kembang Sepatu";
-        Sepatu.iklim = "Tropis";
-        Sepatu.ukuran = "Sedang";
-        Sepatu.Warna_Kelopak = "Merah";
-        Sepatu.Jumlah_Kelopak = 5;
-        Sepatu.Jenis_Kelamin = "Benang Sari";
+        Kertas.nama = "Bunga Kertas";
+        Kertas.iklim = "Tropis";
+        Kertas.ukuran = "Kecil";
+        Kertas.Warna_Kelopak = "Ungu";
+        Kertas.Jumlah_Kelopak = 3;
+        Kertas.Jenis_Kelamin = "Putik";
         
-        Sepatu.nama = "Bunga Kertas";
-        Sepatu.iklim = "Tropis";
-        Sepatu.ukuran = "Kecil";
-        Sepatu.Warna_Kelopak = "Ungu";
-        Sepatu.Jumlah_Kelopak = 3;
-        Sepatu.Jenis_Kelamin = "Putik";
-        
-        Sepatu.nama = "Bunga Melati";
-        Sepatu.iklim = "Tropis";
-        Sepatu.ukuran = "Kecil";
-        Sepatu.Warna_Kelopak = "Putih";
-        Sepatu.Jumlah_Kelopak = -+9;
-        Sepatu.Jenis_Kelamin = "Kepala Putik";
-        
-        Sepatu.CaraKawin();
-        Kertas.CaraKawin();
-        Melati.CaraKawin();
+       
     }
 }
